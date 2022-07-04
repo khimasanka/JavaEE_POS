ac0cd4319c7df670680bb3b51ce0ce9a5511c6cd
Kaveesha Himasanka <kaveee143@gmail.com>
2022-07-04 08:46:14 +0530

    first commit

---
+++ Back_End/src/CustomerServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Kaveesha Himasanka
+ * @since : 0.1.0
+ * Kaveesha Himasanka
+ * 2022
+ **/
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hari");
+    }
+
+}
