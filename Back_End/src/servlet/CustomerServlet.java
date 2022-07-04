842045a4e90483af1972a64e0675fcea18bac4f2
Kaveesha Himasanka <kaveee143@gmail.com>
2022-07-04 12:39:06 +0530

    add customer servlet

--- Back_End/src/servlet/CustomerServlet.java
+++ Back_End/src/servlet/CustomerServlet.java
@@ -1,3 +1,5 @@
+package servlet;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +17,6 @@ import java.io.IOException;
 public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("hari");
+        System.out.println("success");
     }
-
 }
