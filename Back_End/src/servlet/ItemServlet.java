eb4f669b306899e3ec62555ccc191cf199cd4ff8
Kaveesha Himasanka <kaveee143@gmail.com>
2022-07-04 12:43:04 +0530

    item,order,order detail servlets add

---
+++ Back_End/src/servlet/ItemServlet.java
@@ -0,0 +1,14 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author : Kaveesha Himasanka
+ * @since : 0.1.0
+ * Kaveesha Himasanka
+ * 2022
+ **/
+@WebServlet(urlPatterns = "/item")
+public class ItemServlet extends HttpServlet {
+}
---
+++ Back_End/src/servlet/OrderServlet.java
@@ -0,0 +1,15 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.xml.ws.spi.http.HttpContext;
+
+/**
+ * @author : Kaveesha Himasanka
+ * @since : 0.1.0
+ * Kaveesha Himasanka
+ * 2022
+ **/
+@WebServlet(urlPatterns = "/order")
+public class OrderServlet extends HttpServlet {
+}
